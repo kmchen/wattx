f133c3de675e215b6606c28e07dadebb9432754c
kmchen <kmchen@kmchens-MacBook-Pro.local>
2019-02-18 08:52:10 +0100

    Make chart responsive

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react';
+import styled from 'styled-components';
 import {
-    Cell, ScatterChart, Scatter, XAxis, YAxis, ZAxis, CartesianGrid, Tooltip
+    ResponsiveContainer, Cell, ScatterChart, Scatter, XAxis, YAxis, ZAxis, CartesianGrid, Tooltip
 } from 'recharts';
 
 const TooltipInfo = ({ active, payload, label }) => {
@@ -20,33 +21,53 @@ const TooltipInfo = ({ active, payload, label }) => {
 export class Chart extends PureComponent {
   render() {
     return (
-      <ScatterChart
-        width={800}
-        height={600}
-        margin={{ top: 20, right: 20, bottom: 20, left: 200 }}
-      >
-        <XAxis type="number"
-          dataKey="x"
-          unit="$"
-          name="Market Capitalization"
-        />
-        <YAxis
-          type="number"
-          dataKey="y"
-          unit="$"
-          name="Volume" />
-        <ZAxis
-          type="number"
-          dataKey="z"
-          unit="%"
-          name="Price Change" />
-        <Tooltip content={<TooltipInfo />} cursor={{ strokeDasharray: '3 3' }} />
-        <Scatter
-          name="market capitalization v.s. volume"
-          data={this.props.data}
-          fill="#8884d8"
-        />
-      </ScatterChart>
+      <ChartContainer>
+        <ChartArea>
+          <ResponsiveContainer width='60%' height='60%' >
+            <ScatterChart
+              width={300}
+              height={300}
+              margin={{ top: 20, right: 20, bottom: 20, left: 20 }}
+            >
+              <XAxis type="number"
+                dataKey="x"
+                unit="$"
+                name="Market Capitalization"
+              />
+              <YAxis
+                type="number"
+                dataKey="y"
+                unit="$"
+                name="Volume" />
+              <ZAxis
+                type="number"
+                dataKey="z"
+                unit="%"
+                name="Price Change" />
+              <Tooltip content={<TooltipInfo />} cursor={{ strokeDasharray: '3 3' }} />
+              <Scatter
+                name="market capitalization v.s. volume"
+                data={this.props.data}
+                fill="#8884d8"
+              />
+            </ScatterChart>
+          </ResponsiveContainer>
+      </ChartArea>
+    </ChartContainer>
     );
     }
 }
+
+const ChartContainer = styled.div`
+  padding-bottom: 56.25%;
+  position: relative;
+  height: 0;
+`;
+
+const ChartArea = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+`;
