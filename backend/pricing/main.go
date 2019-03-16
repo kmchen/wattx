3343bec075f0a277cdc04c90d19789522d0bd5f5
kmchen <kmchen@kmchens-MacBook-Pro.local>
2019-03-16 10:13:43 +0100

    Add grpc

--- backend/pricing/main.go
+++ backend/pricing/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"sort"
 	"strings"
 	"time"
+
+	pb "github.com/wattx/backend/proto"
+	"google.golang.org/grpc"
 )
 
 type Data struct {
@@ -35,8 +40,11 @@ type Price struct {
 const topAssetsUrl = "https://min-api.cryptocompare.com/data/top/volumes?tsym=USD&limit=500"
 const currencyUrl = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=%s"
 
-const topAssetCachingTime = 120 * time.Second
-const converstionRateLimit = 60 * time.Second
+var headers = map[string]string{
+	"X-CMC_PRO_API_KEY": "101f8864-41d9-4223-9f32-effa9b886491",
+}
+
+const assetUpdateTime = 60 * time.Second
 const batchSize = 10
 
 func isWhiteListed(asset string) bool {
@@ -51,17 +59,7 @@ func isWhiteListed(asset string) bool {
 var DefaultClient = &http.Client{}
 
 func httpGet(url string, headers map[string]string) ([]byte, error) {
-	//resp, err := http.Get(url)
-	//if err != nil {
-	//return nil, err
-	//}
-	//defer resp.Body.Close()
-	//body, err := ioutil.ReadAll(resp.Body)
-	//if err != nil {
-	//return nil, err
-	//}
-	//return body, err
-
+	// Init new request
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		fmt.Printf("Fail to create request, %v\n", err)
@@ -73,6 +71,7 @@ func httpGet(url string, headers map[string]string) ([]byte, error) {
 			req.Header.Add(k, v)
 		}
 	}
+	// Send out request
 	resp, err := DefaultClient.Do(req)
 	if err != nil {
 		fmt.Printf("Fail to send request %v\n", err)
@@ -88,19 +87,22 @@ func httpGet(url string, headers map[string]string) ([]byte, error) {
 }
 
 func getTopAssets(topAssetsChan chan []string) {
-	ticker := time.NewTicker(topAssetCachingTime)
-	for t := range ticker.C {
+	ticker := time.NewTicker(assetUpdateTime)
+	t := time.Time{}
+	for ; true; t = <-ticker.C {
 		// Fetching top 500
 		fmt.Printf("Fetching top 200 assets %v\n", t)
 		resp, err := httpGet(topAssetsUrl, nil)
 		if err != nil {
 			fmt.Printf("Fail to fetch top asset: %v\n", err)
 		}
+		// Unmarshal Asset data
 		crypto := Crypto{}
 		if err := json.Unmarshal(resp, &crypto); err != nil {
 			fmt.Printf("Fail to unmarshal assets: %v\n", err)
 		}
 
+		// Sort by volume
 		sort.Slice(crypto.Data,
 			func(i, j int) bool {
 				return crypto.Data[j].Volume24hourto < crypto.Data[i].Volume24hourto
@@ -119,61 +121,75 @@ func getTopAssets(topAssetsChan chan []string) {
 	}
 }
 
-func getAssetValue(topAssetsChan chan []string, assetValueDoneChan chan bool) {
-	ticker := time.NewTicker(converstionRateLimit)
-	done := make(chan Conversion, 10)
-	for {
-		select {
-		case result := <-done:
-			fmt.Printf("Successful query currency: %v\n", result)
-		case <-ticker.C:
-			topAssets := <-topAssetsChan
-			for i := 0; i < len(topAssets); i += batchSize {
-				assets := topAssets[i : i+batchSize]
-				assetsStr := strings.Join(assets, ",")
-				fmt.Printf("url: %s\n", assetsStr, batchSize)
-				go func(symbols string, done chan Conversion) {
-					var url = fmt.Sprintf(currencyUrl, symbols)
-					var headers = map[string]string{
-						"X-CMC_PRO_API_KEY": "101f8864-41d9-4223-9f32-effa9b886491",
-					}
-					var resp = httpGet(url, headers)
-					//var DefaultClient = &http.Client{}
-					//req, err := http.NewRequest("GET", url, nil)
-					//if err != nil {
-					//fmt.Printf("Fail to create currency request, %v\n", err)
-					//return
-					//}
-					//req.Header.Add()
-					//resp, err := DefaultClient.Do(req)
-					//if err != nil {
-					//fmt.Printf("Fail to get Currency %v\n", err)
-					//return
-					//}
-					//defer resp.Body.Close()
-					//body, err := ioutil.ReadAll(resp.Body)
-					//if err != nil {
-					//fmt.Printf("Fail to read response %v\n", err)
-					//}
-					conversion := Conversion{}
-					err = json.Unmarshal(body, &conversion)
-					if err != nil {
-						fmt.Println("error:", err)
-					}
-					fmt.Printf("%s, %+v\n", symbols, conversion)
-					done <- conversion
-				}(assetsStr, done)
-			}
+func getAssetValue(topAssetsChan chan []string, assetDoneChan chan Conversion) {
+	for topAssets := range topAssetsChan {
+		for i := 0; i < len(topAssets); i += batchSize {
+			assets := topAssets[i : i+batchSize]
+			assetsStr := strings.Join(assets, ",")
+			fmt.Printf("url: %s\n", assetsStr, batchSize)
+			go func(symbols string, done chan Conversion) {
+				var url = fmt.Sprintf(currencyUrl, symbols)
+				var resp, err = httpGet(url, headers)
+				if err != nil {
+					fmt.Println("error:", err)
+					return
+				}
+				conversion := Conversion{}
+				err = json.Unmarshal(resp, &conversion)
+				if err != nil {
+					fmt.Println("error:", err)
+					return
+				}
+				done <- conversion
+			}(assetsStr, assetDoneChan)
 		}
 	}
 }
 
+const (
+	address     = "localhost:50051"
+	defaultName = "world"
+)
+
 func main() {
 	topAssetsChan := make(chan []string)
-	assetValueDoneChan := make(chan bool)
+	assetValueDoneChan := make(chan Conversion)
 	go getAssetValue(topAssetsChan, assetValueDoneChan)
 	go getTopAssets(topAssetsChan)
-	<-assetValueDoneChan
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewGreeterClient(conn)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	for value := range assetValueDoneChan {
+		fmt.Printf("%v\n", value)
+		pbPrice := pb.Price{
+			Price: float32(1.1),
+		}
+		pbQuote := map[string]*pb.Price{
+			"USD": &pbPrice,
+		}
+		pbCurrency := pb.Currency{
+			Quote:  pbQuote,
+			Symbol: "hello",
+		}
+		pbData := map[string]*pb.Currency{
+			"hello": &pbCurrency,
+		}
+		pbConversion := pb.Conversion{
+			Data: pbData,
+		}
+		r, err := c.UpdateAsset(ctx, &pbConversion)
+		if err != nil {
+			log.Fatalf("could not greet: %v", err)
+		}
+		log.Printf("Greeting: %s", r.Status)
+	}
 }
 
 var whiteList = []string{
--- backend/ranking/main.go
+++ backend/ranking/main.go
@@ -1,74 +1,34 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"net/http"
-	"sort"
+	"context"
+	"log"
+	"net"
+
+	pb "github.com/wattx/backend/proto"
+	"google.golang.org/grpc"
 )
 
-type Data struct {
-	Symbol         string  `json:"symbol"`
-	Volume24hourto float32 `json:"Volume24hourto"`
-}
+const (
+	port = ":50051"
+)
 
-type Crypto struct {
-	Data []Data
-}
+// server is used to implement helloworld.GreeterServer.
+type server struct{}
 
-type Conversion struct {
-	Data map[string]Currency
+func (s *server) UpdateAsset(ctx context.Context, in *pb.Conversion) (*pb.Reply, error) {
+	log.Printf("Received: %v", in.Data)
+	return &pb.Reply{Status: "OK "}, nil
 }
-
-type Currency struct {
-	Quote map[string]Price `json: "quote"`
-}
-
-type Price struct {
-	Price float32 `json: "price"`
-}
-
-const top200AssetsUrl = "https://min-api.cryptocompare.com/data/top/volumes?tsym=USD&limit=210"
 
 func main() {
-	resp, _ := httpGet(top200AssetsUrl)
-	crypto := Crypto{}
-	err := json.Unmarshal(resp, &crypto)
+	lis, err := net.Listen("tcp", port)
 	if err != nil {
-		fmt.Println("error:", err)
+		log.Fatalf("failed to listen: %v", err)
 	}
-
-	sort.Slice(crypto.Data, func(i, j int) bool { return crypto.Data[j].Volume24hourto < crypto.Data[i].Volume24hourto })
-
-	//var done = make(chan bool, 210)
-	for _, v := range crypto.Data {
-		const currencyUrl = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=%s"
-		var url = fmt.Sprintf(currencyUrl, v.Symbol)
-		var DefaultClient = &http.Client{}
-		req, _ := http.NewRequest("GET", url, nil)
-		req.Header.Add("X-CMC_PRO_API_KEY", "101f8864-41d9-4223-9f32-effa9b886491")
-		resp2, _ := DefaultClient.Do(req)
-		defer resp2.Body.Close()
-		body, _ := ioutil.ReadAll(resp2.Body)
-		conversion := Conversion{}
-		err := json.Unmarshal(body, &conversion)
-		if err != nil {
-			fmt.Println("error:", err)
-		}
-		fmt.Printf("%v", conversion)
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
 	}
-}
-
-func httpGet(url string) ([]byte, error) {
-	resp, err := http.Get(url)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-	return body, err
 }
